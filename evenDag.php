5fe6cee2dd4f1ebdbf9916e013b88573e4eb950a
Bashar <123710@student.drenthecollege.nl>
2019-03-12 09:15:19 +0100

    code aangepast.

--- evenDag.php
+++ evenDag.php
@@ -1,15 +1,25 @@
-<?php
-function evenDag()
-{
-  $datum = date("d");
-  echo "De datum van vandaag is $datum"."<br>";
-  if ($datum % 2 == 0){
-    echo " het is een even getal";
-}
-    else {
-      echo " het is een oneven getal";
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    function evenDag()
+    {
+      $datum = date("d");
+      echo "De datum van vandaag is $datum"."<br>";
+      if ($datum % 2 == 0){
+        echo " het is een even getal";
+    }
+        else {
+          echo " het is een oneven getal";
 
-  }
-}
-evendag();
- ?>
+      }
+    }
+    evendag();
+     ?>
+  </body>
+</html>
