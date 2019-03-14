7f06083a66223bef2f0d1b1bdb10b025f57edb00
Bashar <123710@student.drenthecollege.nl>
2019-03-14 11:38:38 +0100

    getPHPosInfo opdracht gemaakt

--- getPHPosInfo.php
+++ getPHPosInfo.php
@@ -6,32 +6,11 @@
     <title></title>
   </head>
   <body>
-
     <?php
-    // echo php_uname();
-    // echo PHP_OS;
-    // if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    //     echo 'This is a server using Windows!';
-    // } else {
-    //     echo 'This is a server not using Windows!';
-    // }
-
     function getPHPosinfo(){
       return php_uname('a');
     }
-
     echo getPHPosinfo();
-
-    echo "<br><br><br>";
-
-    echo PHP_OS;
-
-    echo "<br><br><br>";
-
-    echo DIRECTORY_SEPARATOR;
-
-
     ?>
-
   </body>
 </html>
