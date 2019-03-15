0ebd8ec9dd2aa2e5ff96adf6ffa5c96f65ee29c7
Bashar <123710@student.drenthecollege.nl>
2019-03-15 08:48:39 +0100

    getMax oefening gemaakt

---
+++ getMax.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      function getMax(){
+      $integer = array(2, 9, 4, 7);
+      return max($integer);
+      }
+      echo getMax();
+     ?>
+
+  </body>
+</html>
