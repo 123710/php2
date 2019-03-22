fedca775a05176d5165c11910dc3bd8f49cb5023
Bashar <123710@student.drenthecollege.nl>
2019-03-22 11:32:31 +0100

    faculteitberekenen oefening gemaakt

---
+++ faculteitBerekenen.php
@@ -0,0 +1,13 @@
+<?php
+function faculteit($getal)
+{
+    $uitkomst = 1;
+    while($getal > 0)
+    {
+        $uitkomst *= $getal;
+        $getal--;
+    }
+    return $uitkomst;
+}
+echo faculteit(4);
+?>
